c7d6b4979aceec1e5ffddeeaaa05847b24ca9328
Aarti Yadav <aartiyadav@Aartis-MacBook-Pro.local>
2021-07-26 20:18:10 +0530

    mountain animation changes

--- src/app/mt-cd/mt-cd.page.ts
+++ src/app/mt-cd/mt-cd.page.ts
@@ -28,57 +28,39 @@ export class MtCdPage implements OnInit {
   public x:number;
   public y:number;
 
-  private movement = {
-    horizontal : 'horizontal',
-    vertical : 'vertical'
-  }
-
   private mountainLimits = {
-    extremeLeft : -64,
-    extremeRight : 0,
-    horizontalMiddle : -32,
-    extremeTop : -32,
-    extremeBottom : 0,
-    verticalMiddle : -16,
-    steps: 8
+    movementConstant : 1.4
   }
 
   private skyLimits = {
-    extremeLeft : -32,
-    extremeRight : 0,
-    horizontalMiddle : -16,
-    extremeTop : -32,
-    extremeBottom : 0,
-    verticalMiddle : -16,
-    steps: 8
+    movementConstant : 0.65
   }
-
-  public test : any;
 
   constructor(public deviceMotion: DeviceMotion, private deviceOrientation: DeviceOrientation) {}
 
   start() {
     try {
       var option: DeviceMotionAccelerometerOptions = {
-        frequency: 800
+        frequency: 10
       };
       this.idxyp = this.deviceMotion.watchAcceleration(option).subscribe((acc: DeviceMotionAccelerationData) => {
 
-        this.x = Math.trunc(acc.x);
-        this.y = Math.trunc(acc.y);
+        this.x = acc.x;
+        this.y = acc.y;
 
         let mountain = {
-          left : this.calculate(this.movement.horizontal, this.x, this.mountainLimits),
-          top : this.calculate(this.movement.vertical, this.y, this.mountainLimits)
+          left : this.calculate(this.x, this.mountainLimits,false),
+          top : this.calculate(this.y, this.mountainLimits,false)
         }
 
         let sky = {
-          left : this.calculate(this.movement.horizontal, this.x, this.skyLimits, true),
-          top : this.calculate(this.movement.vertical, this.y, this.skyLimits, true)
+          left : this.calculate(this.x, this.skyLimits, true,),
+          top : this.calculate(this.y, this.skyLimits, true)
         }
 
         this.setPosition(this.card3, mountain);
-        this.setPosition(this.card4, sky);
+        // this.setPosition(this.card4, sky);
+
         }
 
       )
@@ -91,48 +73,12 @@ export class MtCdPage implements OnInit {
     this.idxyp.unsubscribe();
   }
 
-  calculate(movement, value, limits, invertDirection = false){
-    let valueSign = Math.sign(value);
-    let positionGap = movement == this.movement.horizontal ? 4 : 2;
-    let returnValue : number;
-    if(!value){
-      returnValue = (movement == this.movement.horizontal) ? limits.horizontalMiddle : limits.verticalMiddle;
-    } else {
-      let startValue = movement == this.movement.horizontal ? limits.horizontalMiddle : limits.verticalMiddle;
-      let endValue : number;
-      if(invertDirection){
-        endValue = movement == this.movement.horizontal ? (valueSign == -1 ? limits.extremeRight : limits.extremeLeft)
-        :  (valueSign == -1 ? limits.extremeBottom : limits.extremeTop);
-      } else {
-        endValue = movement == this.movement.horizontal ? (valueSign == -1 ? limits.extremeLeft : limits.extremeRight)
-        :  (valueSign == -1 ? limits.extremeTop : limits.extremeBottom);
-      }
-
-      let postionValues = this.getAxisPoints(startValue, endValue);
-      returnValue = postionValues[ Math.abs(value) * positionGap ] ;
-      returnValue = returnValue ? returnValue : postionValues[postionValues.length - 1];
-    }
-    return returnValue;
-  }
-
-  getAxisPoints(startValue, endValue){
-    var list = [];
-    let endValueSign = Math.sign(endValue);
-    if(endValueSign == -1){
-      for (var i = startValue; i >= endValue ; i--) {
-          list.push(i);
-      }
-    } else {
-      for (var i = startValue; i <= endValue ; i++) {
-          list.push(i);
-      }
-    }
-    return list;
+  calculate(value, limits, invertDirection = false){
+    return invertDirection ? -(value) * limits.movementConstant : value * limits.movementConstant;
   }
 
   setPosition(card, value){
-    card.style.left =  value.left + 'px';
-    card.style.top =  value.top + 'px';
+    card.style.transform = "translate(" + value.left +"%, "+ value.top + "%)"
   }
 
   ngOnInit() {
